5d289db98950aa0f228d89df4f23b2fed24ba6c6
Bhavesh Shinde <150414943+7S-Bhavesh@users.noreply.github.com>
2026-07-28 17:44:33 +0530

    Authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font bold text-gradient">Resumemind</p>
+      </Link>
+
+      <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+        Upload Resume
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,48 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+// import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Resume mind" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+
+  const {  auth } = usePuterStore();
+const navigate = useNavigate();
+
+useEffect(() => {
+  if (!auth.isAuthenticated) navigate("/auth?next=/");
+}, [auth.isAuthenticated]);
+    
+  
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your resume applications and ratings </h1>
+          <h2>Review your submission</h2>
+        </div>
+
+      {resumes.length > 0 && (
+        <div className="resume-sections">
+          {resumes.map((resume) => (
+            <div>
+              <ResumeCard key={resume.id} resume={resume} />
+            </div>
+          ))}
+        </div>
+      )}
+      </section>
+    </main>
+  );
 }
